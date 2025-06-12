2b58503b5d6275f8bd7667ea771a45fa272e5a9f
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-12 18:55:12 +0100

    2 states of purchase in view

--- src/purchases/views.py
+++ src/purchases/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+from products.models import Product
+from .models import Purchase
+
+
+def purchase_start(request):
+    if not request.method == "POST":
+        return HttpResponseBadRequest()
+    if not request.user.is_authenticated:
+        return HttpResponseBadRequest()
+    
+    handle = request.POST.get("handle")
+    obj = Product.objects.get(handle=handle)
+    purchase = Purchase.objects.create(user=request.user, product=obj)
+
+    request.session["purchase_id"] = purchase.id
+
+    return HttpResponse("Started")
+
+
+def purchase_success(request):
+    purchase_id = request.session.get("purchase_id")
+    if purchase_id:
+        purchase = Purchase.objects.get(id=purchase_id)
+        purchase.completed = True
+        purchase.save()
+    
+    return HttpResponse("Success")
+
+
+def purchase_stopped(request):
+    return HttpResponse("Stopped")
