24acd7b55293a0ffcb81e49e5bc39c0e76e6e0af
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-21 22:42:36 +0100

    stripe is working

--- src/products/utils.py
+++ src/products/utils.py
@@ -7,21 +7,26 @@ stripe.api_key = STRIPE_SECRET_KEY
 
 
 def product_sales_pipeline(product_name="Test product", product_price=1000):
+    # 1. Create the Product
     stripe_product_obj = stripe.Product.create(name=product_name)
     stripe_product_id = stripe_product_obj.id
-    stripe_price_obj = stripe.Product.create(
+
+    # 2. Create the Price (associated with the Product)
+    stripe_price_obj = stripe.Price.create(
         product=stripe_product_id,
         unit_amount=product_price,
-        currency="usd", 
+        currency="usd",
     )
     stripe_price_id = stripe_price_obj.id
 
+    # 3. Define URLs for the Checkout Session
     base_endpoint = "http://127.0.0.1:8000"
     success_url = f"{base_endpoint}/purchases/success/"
     cancel_url = f"{base_endpoint}/purchases/stopped/"
 
+    # 4. Create the Checkout Session
     checkout_session = stripe.checkout.Session.create(
-        line_items= [
+        line_items=[
             {
                 "price": stripe_price_id,
                 "quantity": 1,
@@ -29,7 +34,7 @@ def product_sales_pipeline(product_name="Test product", product_price=1000):
         ],
         mode="payment",
         success_url=success_url,
-        cancel_url=cancel_url
+        cancel_url=cancel_url,
     )
 
     print(checkout_session.url)
