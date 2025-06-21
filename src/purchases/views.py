e788bd43f8f37ad3a798073e41b8addcf11607e2
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-21 23:07:40 +0100

    stripe integration v1

--- src/purchases/views.py
+++ src/purchases/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
 import random 
+import stripe 
 
 from products.models import Product
 from .models import Purchase
+
+from micro_ecom.env import config
+
+
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default=None)
+stripe.api_key = STRIPE_SECRET_KEY
+
+BASE_ENDPOINT = "http://127.0.0.1:8000"
 
 
 def purchase_start(request):
@@ -14,17 +25,44 @@ def purchase_start(request):
     
     handle = request.POST.get("handle")
     obj = Product.objects.get(handle=handle)
-    purchase = Purchase.objects.create(user=request.user, product=obj)
 
+    stripe_price_id = obj.stripe_price_id
+    if stripe_price_id is None:
+        return HttpResponseBadRequest()
+    
+    purchase = Purchase.objects.create(user=request.user, product=obj)
     request.session["purchase_id"] = purchase.id
 
-    number = random.randint(0, 1)
-    print(number)
+    #Stripe checkout
+    success_path = reverse('purchases:success')
+    if not success_path.startswith("/"):
+        success_path = f"/{success_path}"
 
-    if number == 1:
-        return HttpResponseRedirect ('/purchases/success')
+    cancel_path = reverse('purchases:stopped')
+    if not cancel_path.startswith("/"):
+        cancel_path = f"/{cancel_path}"
+
+    success_url = f"{BASE_ENDPOINT}{success_path}/"
+    cancel_url = f"{BASE_ENDPOINT}{cancel_path}/"
+
+    print(success_url, cancel_url)
+
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[
+            {
+                "price": stripe_price_id,
+                "quantity": 1,
+            }
+        ],
+        mode="payment",
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
     
-    return HttpResponseRedirect ('/purchases/stopped')
+    purchase.stripe_checkout_session_id = checkout_session.id
+    purchase.save()
+    
+    return HttpResponseRedirect(checkout_session.url)
 
 
 def purchase_success(request):
