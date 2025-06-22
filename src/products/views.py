02e92afb85e3092a946acb8dbbeded6609e14f8c
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-06-22 16:01:30 +0100

    presigning files to access from storage

--- src/products/views.py
+++ src/products/views.py
@@ -1,12 +1,12 @@
 import mimetypes
 
-from django.http import FileResponse, Http404, HttpResponseBadRequest
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+
 from .models import Product, ProductAttachment
 from .forms import ProductForm, ProductUpdateForm, ProductAttachmentInlineFormSet
-
+from micro_ecom.storages.utils import generate_presigned_url
 
 def product_create(request):
     context = {}
@@ -95,19 +95,17 @@ def product_attachment_download(request, handle=None, pk=None):
     attachment = get_object_or_404(ProductAttachment, product__handle=handle, pk=pk)
     can_download = False
     can_download = attachment.is_free or False
-    if request.user.is_authenticated:
-        can_download = True
+    if request.user.is_authenticated and can_download is False:
+        can_download = request.user.purchase_set.filter(product=attachment.product, completed=True).exists()
     if can_download is False:
         return HttpResponseBadRequest()
 
-    try:
-        file = attachment.file.open(mode="rb")
-    except FileNotFoundError:
-        raise Http404("File not found.")
+    file_name = attachment.file.name
+    file_url = generate_presigned_url(file_name)
 
-    filename = attachment.file.name
-    content_type, _ = mimetypes.guess_type(filename)
-    response = FileResponse(file)
-    response["Content-Type"] = content_type or "application/octet-stream"
-    response["Content-Disposition"] = f"attachment;filename={filename}"
-    return response
+    # filename = attachment.file.name
+    # content_type, _ = mimetypes.guess_type(filename)
+    # response = FileResponse(file)
+    # response["Content-Type"] = content_type or "application/octet-stream"
+    # response["Content-Disposition"] = f"attachment;filename={filename}"
+    return HttpResponseRedirect(file_url)
